6c6db1e21149c5d964b683199be092dd038080a9
zacharyabolton <zacbolton2129@gmail.com>
2025-01-30 14:35:39 -0300

    Adds `P1` & `P2` thresholds
    With values matching Ibrahim et al. (2023)

--- src/constants.py
+++ src/constants.py
@@ -14,6 +14,11 @@ from pathlib import Path
 ROOT_DIR = os.path.dirname(Path(__file__).parent)
 # https://huggingface.co/sentence-transformers/all-MiniLM-L12-v2
 MODEL = 'sentence-transformers/all-MiniLM-L12-v2'
-# Learning rate of 2e-05 as per Ibrahim Et. Al (2023) [13:10]
+# Learning rate of 2e-05 as per Ibrahim et al. (2023) [8:10]
 # This is intentionally very small as we are fine-tuning a pre-trained model
 INITIAL_LEARNING_RATE = 2e-05
+# p1 and p2 decision threshold, between which the model collapses values
+# to 0.5, meaning "I don't know". These are set to 0.45 and 0.54
+# respectively, matching Ibrahim et al. (2023) [8:9-11].
+P1 = 0.45
+P2 = 0.54
