62688f16de48062571037cdb0280c764598f3754
zacharyabolton <zacbolton2129@gmail.com>
2025-02-02 18:16:55 -0300

    Updates grid search routine for margin search

--- src/grid_search.sh
+++ src/grid_search.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Hyperparameters to vary
-margins=(0.45 0.5 0.55)
-learning_rates=(0.00001 0.00002 0.00003)
-clip_values=(0.5 1.0 1.5)
+margin_ss=(0.2 0.25 0.3 0.35 0.4)
+margin_ds=(0.4 0.45 0.5 0.55 0.6)
 
 # Fixed hyperparameters (based on prior experiments)
 batch_size=8
@@ -13,36 +12,36 @@ epsilon=0.000001
 num_pairs=4096
 num_folds=2
 num_epochs=1
+initial_lr=0.00002
 seed=0
+max_norm=1.0
 
-for margin in "${margins[@]}"; do
-    for lr in "${learning_rates[@]}"; do
-        for clip in "${clip_values[@]}"; do
-            margin_str=$(echo $margin | tr '.' '_')
-            lr_str=$(echo $lr | tr '.' '_')
-            clip_str=$(echo $clip | tr '.' '_')
-            exp_name="margin${margin_str}_lr${lr_str}_clip${clip_str}"
+for margin_s in "${margin_ss[@]}"; do
+    for margin_d in "${margin_ds[@]}"; do
+        margin_s_str=$(echo $margin_s | tr '.' '_')
+        margin_d_str=$(echo $margin_d | tr '.' '_')
+        exp_name="margin_s${margin_s_str}_margin_d${margin_d_str}"
+        
+        echo "%%%%%%%%%%%% starting ${exp_name}"
+        
+        python3 main.py \
+            "$exp_name" \
+            "../data/normalized" \
+            $batch_size \
+            $accum_steps \
+            $chunk_size \
+            $margin_s \
+            $margin_d \
+            $epsilon \
+            $num_pairs \
+            $num_folds \
+            $num_epochs \
+            $initial_lr \
+            -s $seed \
+            -m $max_norm
             
-            echo "%%%%%%%%%%%% starting ${exp_name}"
-            
-            python3 main.py \
-                "$exp_name" \
-                "../data/normalized" \
-                $batch_size \
-                $accum_steps \
-                $chunk_size \
-                $margin \
-                $epsilon \
-                $num_pairs \
-                $num_folds \
-                $num_epochs \
-                $lr \
-                -s $seed \
-                -m $clip
-                
-            echo "%%%%%%%%%%%% ending ${exp_name}"
-            echo ""
-        done
+        echo "%%%%%%%%%%%% ending ${exp_name}"
+        echo ""
     done
 done
 
