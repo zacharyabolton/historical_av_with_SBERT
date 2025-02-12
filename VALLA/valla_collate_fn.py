35545cb234c5dd7a5c4df802bf5bc7e3bf90c58b
zacharyabolton <zacbolton2129@gmail.com>
2025-02-11 23:26:29 -0300

    Adds docstring

--- VALLA/valla_collate_fn.py
+++ VALLA/valla_collate_fn.py
@@ -24,10 +24,25 @@ from constants import MODEL
 def valla_collate_fn(batch):
     """
     Collate function to properly batch the paired inputs.
+
+    :param batch: A list of `sentence-transformers` `InputSamples` with
+    members `label` (int) and `texts` (list), providing a
+    same/different-author pair with ground truth label.
+    :type batch: list
+
+    :returns: a 3-tuple with the first two elements of type dict having
+    the shape of an PyTorch tokenizer embedding, and the last being an int
+    represeting ground-truth.
     """
+    # Tokenize
     tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
+    # Build tensor of of labels
     labels = torch.tensor([item.label for item in batch])
+    # Build list of toenization embeddings truncating beyond 512 to match
+    # this project's LILA formulation, and zero-padding if under (which
+    # should not occur given the recieved plain text is guaranteed to be
+    # of word length 512).
     anchor_texts = [tokenizer(item.texts[0],
                               return_tensors="pt",
                               padding='max_length',
@@ -39,7 +54,8 @@ def valla_collate_fn(batch):
                              truncation=True,
                              max_length=512) for item in batch]
 
-    # Combine input_ids and attention_masks
+    # Combine input_ids and attention_masks in a form ingestible by class
+    # torch.nn.Module
     batched_a = {
         'input_ids': torch.cat([x['input_ids'] for x in anchor_texts]),
         'attention_mask': torch.cat([x['attention_mask']
